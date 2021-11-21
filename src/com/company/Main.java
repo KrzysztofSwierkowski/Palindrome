175fd6d0a2ccf783ff1bffe2aec52cddfa45e1d3
KrzysztofSwierkowski <Swierkowski.K@gmail.com>
2021-11-21 21:10:15 +0100

    completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static boolean checkText (String text){
+
+        StringBuilder text1 = new StringBuilder();
+        text1.append(text);
+        text1.reverse();
+        if (text1.toString().equals(text.toString()) == true){
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter the text: ");
+        String palindrome = scanner.nextLine();
+        if (checkText(palindrome) == true){
+            System.out.printf("%s is palindrome",palindrome);
+        }
+        else {
+            System.out.printf("%s isn't palindrome",palindrome);
+        }
+
     }
 }
